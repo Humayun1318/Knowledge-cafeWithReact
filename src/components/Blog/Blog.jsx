be6090d541bd76fad4f34ffb78a1f05324f3f067
Humayun1318 <humayunKabir6267@gmail.com>
2023-03-31 01:21:06 +0600

    single part data loaded done

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -5,20 +5,26 @@ const Blog = (props) => {
     const {id, authorName, blogTitle, images, readTime, publishDate}= props.blog;
     console.log(authorName)
     return (
-        <div>
+        <div className='blog-single-part'>
             <img className='blog-cover-image' src={images.blogCoverImage} alt="" />
             <div className='blog-mid-container'>
                 <div className='profile-flex'>
                     <img src={images.authorImage} alt="" />
                     <div>
-                        <h3>{ authorName}</h3>
-                        <p>{ publishDate}</p>
+                        <h3 className='author-name'>{ authorName}</h3>
+                        <p className='date'>{ publishDate}</p>
                     </div>
                 </div>
                 <div>
-                    <p>{ readTime} min read <button></button></p>
+                    <p>{ readTime} min read <button>mark</button></p>
                 </div>
             </div>
+            <div className='title-part-bottom'>
+                <h1 className='blog-title'>{blogTitle}</h1>
+                <p className='blog-title-part-1'>#beginners<span className='title-under-text'>     #programming </span></p>
+                <a className='read-btn' href="">Mark as read</a>
+            </div>
+            <hr />
         </div>
     );
 };
