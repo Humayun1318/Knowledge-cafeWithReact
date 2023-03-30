8c20bbe5cb3c9455e7039c3e56f2e47b16bb9b06
Humayun1318 <humayunKabir6267@gmail.com>
2023-03-30 22:33:42 +0600

    Header complete

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css'
+import profile from "../../image/profile.png"
+const Header = () => {
+    return (
+        <div className='header-container'>
+            <nav className='header'>
+                <div>
+                    <h2 className='header-logo'>Knowledge Cafe</h2>
+                </div>
+                <a href=""><img className='profile-image' src={profile} alt="Images Of profile" /></a>
+
+            </nav>
+            <hr />
+       </div>
+    );
+};
+
+export default Header;
